15a7999706d9787b5c32f50eed2a01c660b9adff
Bartek <bbogucki7@gmail.com>
2024-10-15 10:11:45 +0200

    created accordions and potential plan how it will looking

--- src/components/features/DestinationContainer.tsx
+++ src/components/features/DestinationContainer.tsx
@@ -1,29 +1,59 @@
-import { Container, Accordion } from "react-bootstrap";
+import { Container, Row, Col, Form, Accordion, InputGroup, Button } from "react-bootstrap";
 
 const DestinationContainer: React.FC = () => {
 	return (
 		<Container className='destination-container pt-5'>
-			<Accordion>
-				<Accordion.Item eventKey='0'>
-					<Accordion.Header>Accordion1</Accordion.Header>
-					<Accordion.Body>
-						<div>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sint ducimus nesciunt consectetur explicabo
-							adipisci dignissimos impedit. Consequatur, quis quibusdam.
-						</div>
-					</Accordion.Body>
-				</Accordion.Item>
-
-				<Accordion.Item eventKey='1'>
-					<Accordion.Header>Accordion2</Accordion.Header>
-					<Accordion.Body>
-						<div>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sint ducimus nesciunt consectetur explicabo
-							adipisci dignissimos impedit. Consequatur, quis quibusdam.
-						</div>
-					</Accordion.Body>
-				</Accordion.Item>
-			</Accordion>
+			<Row>
+				<Col xl={10} className='m-auto'>
+					<Form className='destination-container__main-container'>
+						<Form.Text className='p-3 d-flex justify-content-center'>Journey Name</Form.Text>
+						<Accordion>
+							<Accordion.Item eventKey='0'>
+								<Accordion.Header>Travel Plan</Accordion.Header>
+								<Accordion.Body>
+									<InputGroup>
+										<Form.Control></Form.Control>
+										<Button>Add</Button>
+									</InputGroup>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+						<Accordion>
+							<Accordion.Item eventKey='1'>
+								<Accordion.Header>Must-Have Items</Accordion.Header>
+								<Accordion.Body>
+									<InputGroup>
+										<Form.Control></Form.Control>
+										<Button>Add</Button>
+									</InputGroup>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+						<Accordion>
+							<Accordion.Item eventKey='2'>
+								<Accordion.Header>Travel Budget and Estimated Costs</Accordion.Header>
+								<Accordion.Body>
+									<InputGroup>
+										<Form.Control></Form.Control>
+										<Button>Add</Button>
+									</InputGroup>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+						<Accordion>
+							<Accordion.Item eventKey='3'>
+								<Accordion.Header>Trip Photos and Memories</Accordion.Header>
+								<Accordion.Body>
+									<InputGroup>
+										<Form.Control></Form.Control>
+										<Button>Add</Button>
+									</InputGroup>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</Form>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
