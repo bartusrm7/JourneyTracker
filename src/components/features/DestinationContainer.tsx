3009163ee40c2d0d8189db5f7f51a0727fcab979
Bartek <bbogucki7@gmail.com>
2024-10-15 10:25:35 +0200

    connected all accordions in one component and created DestinationAndPlan component

--- src/components/features/DestinationContainer.tsx
+++ src/components/features/DestinationContainer.tsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col, Form, Accordion, InputGroup, Button } from "react-bootstrap";
+import STRUCTURES from "../shared-components/DestinationAndPlan";
 
 const DestinationContainer: React.FC = () => {
 	return (
@@ -7,50 +8,19 @@ const DestinationContainer: React.FC = () => {
 				<Col xl={10} className='m-auto'>
 					<Form className='destination-container__main-container'>
 						<Form.Text className='p-3 d-flex justify-content-center'>Journey Name</Form.Text>
-						<Accordion>
-							<Accordion.Item eventKey='0'>
-								<Accordion.Header>Travel Plan</Accordion.Header>
-								<Accordion.Body>
-									<InputGroup>
-										<Form.Control></Form.Control>
-										<Button>Add</Button>
-									</InputGroup>
-								</Accordion.Body>
-							</Accordion.Item>
-						</Accordion>
-						<Accordion>
-							<Accordion.Item eventKey='1'>
-								<Accordion.Header>Must-Have Items</Accordion.Header>
-								<Accordion.Body>
-									<InputGroup>
-										<Form.Control></Form.Control>
-										<Button>Add</Button>
-									</InputGroup>
-								</Accordion.Body>
-							</Accordion.Item>
-						</Accordion>
-						<Accordion>
-							<Accordion.Item eventKey='2'>
-								<Accordion.Header>Travel Budget and Estimated Costs</Accordion.Header>
-								<Accordion.Body>
-									<InputGroup>
-										<Form.Control></Form.Control>
-										<Button>Add</Button>
-									</InputGroup>
-								</Accordion.Body>
-							</Accordion.Item>
-						</Accordion>
-						<Accordion>
-							<Accordion.Item eventKey='3'>
-								<Accordion.Header>Trip Photos and Memories</Accordion.Header>
-								<Accordion.Body>
-									<InputGroup>
-										<Form.Control></Form.Control>
-										<Button>Add</Button>
-									</InputGroup>
-								</Accordion.Body>
-							</Accordion.Item>
-						</Accordion>
+						{STRUCTURES.map((structure, index) => (
+							<Accordion>
+								<Accordion.Item eventKey='0'>
+									<Accordion.Header key={index}>{structure.label}</Accordion.Header>
+									<Accordion.Body>
+										<InputGroup>
+											<Form.Control></Form.Control>
+											<Button>Add</Button>
+										</InputGroup>
+									</Accordion.Body>
+								</Accordion.Item>
+							</Accordion>
+						))}
 					</Form>
 				</Col>
 			</Row>
---
+++ src/components/shared-components/DestinationAndPlan.ts
@@ -0,0 +1,8 @@
+const STRUCTURES = [
+	{ label: "Travel Plan" },
+	{ label: "Must-Have Items" },
+	{ label: "Travel Budget and Estimated Costs" },
+	{ label: "Trip Photos and Memories" },
+];
+
+export default STRUCTURES;
